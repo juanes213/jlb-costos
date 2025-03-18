bd1a28d702a28eac63acb3c53dee71135fabf847
juanes213 <92051431+juanes213@users.noreply.github.com>
2025-03-18 10:29:56 -0500

    Update types.ts

--- src/contexts/auth/types.ts
+++ src/contexts/auth/types.ts
@@ -21,5 +21,6 @@ export const MOCK_USERS: Record<string, { password: string; role: "admin" | "sto
   "gerenciacomercial@jorgebedoya.com": { password: "Valentino280606", role: "admin" },
   "adminjlb2002": { password: "adminjlb2002", role: "admin" },
   "doperaciones@jorgebedoya.com": {password: "Dojlb2025", role: "admin"},
-  "cfinanciero@jorgebedoya.com": {password: "cfinanciero453", role: "admin"}
+  "cfinanciero@jorgebedoya.com": {password: "cfinanciero453", role: "admin"},
+  "cps@jorgebedoya.com": {password: "chj1234567", role: "admin"}
 };
